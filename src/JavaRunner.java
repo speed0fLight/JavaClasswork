cea9b111c82faf1983336dd07e1a716c468a77d2
speed0fLight <114781935+speed0fLight@users.noreply.github.com>
2025-10-15 19:40:20 -0400

    Finished program 1

--- src/JavaRunner.java
+++ src/JavaRunner.java
@@ -1,5 +1,7 @@
 public class JavaRunner {
     public static void main(String[] args) {
-        
+        String[] dummy = {"f","F"};
+        System.out.println("Ran");
+        FileAndOutput.main(dummy);
     }
 }
